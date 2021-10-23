d8b02a88aad6937646c4ef49995b1e422fa7eb4e
Random Guy <leekhoi5319990@gmail.com>
2021-10-23 21:45:45 +0700

    wrapping up

--- src/Star.js
+++ src/Star.js
@@ -2,5 +2,14 @@ import React from 'react'
 import { FaStar } from 'react-icons/fa'
 
 export default function Star({ selected = false, onSelect = (f) => f }) {
-	return <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />
+	return (
+		<>
+			<h1>Great Star</h1>
+			<FaStar
+				id="star"
+				color={selected ? 'red' : 'grey'}
+				onClick={onSelect}
+			/>
+		</>
+	)
 }
--- src/Star.test.js
+++ src/Star.test.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
 import Star from './Star'
 
 test('renders a star', () => {
 	const div = document.createElement('div')
 	ReactDOM.render(<Star />, div)
-	expect(div.querySelector('svg')).toBeTruthy()
+	expect(div.querySelector('svg')).toHaveAttribute('id', 'star')
+})
+
+test('renders an h1', () => {
+	//We want the component to render, and now we want to test to see if the h1 contains the correct text
+	const { getByText } = render(<Star />) // render() returns an object of queries that can be used to check in with values in that component or element
+	const h1 = getByText(/Great Star/) // {getByText} is the query, which will find the h1 element via the regular expression that’s passed to it and throw an error if no elements match
+	expect(h1).toHaveTextContent('Great Star') // toHaveTextContent() asserts what text the h1 should include
 })
