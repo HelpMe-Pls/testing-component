7842eb899bcad1ab9dfc8d87a0439ecf09419290
Random Guy <leekhoi5319990@gmail.com>
2021-10-23 17:51:14 +0700

    setups

---
+++ src/Star.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Star from './Star'
+
+test('renders a star', () => {
+	const div = document.createElement('div')
+	ReactDOM.render(<Star />, div)
+})
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { render } from "react-dom";
-import ColorProvider from "./ColorProvider"; // must have
-import App from "./App";
+import React from 'react'
+import { render } from 'react-dom'
+import ColorProvider from './ColorProvider' // must have
+import App from './App'
 
 render(
-  <ColorProvider>
-    <App />
-  </ColorProvider>,
-  document.getElementById("root")
-);
+	<ColorProvider>
+		<App />
+	</ColorProvider>,
+	document.getElementById('root')
+)
+
+import Star from './Star'
+render(<Star />, document.getElementById('root'))
